49b3e9d9ff0291dded1d2bf198fafbb475559617
SasaG <sasagrabnar.com>
2023-04-17 19:34:08 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
+
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
+
+function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      url: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      age: 37,
+      note: "Allegeric to staying on the same team"
+    }
+  ])
+
+  // {
+  //   name: "Kobe Bryant",
+  //     url: "",
+  //       age: 37
+  // }
+
+  return (
+    <div className="App">
+      <h1>People Invited to my Party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
+    </div>
+  );
+}
+
+export default App;
